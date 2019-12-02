d5ad5847e7a75de6fb784182d2fa3cd22f273ce2
adamg94 <adamgulyas@outlook.com>
2019-12-02 20:48:53 +0100

    hibajavítás

--- gameCode/pipes.js
+++ gameCode/pipes.js
@@ -6,13 +6,15 @@ function Pipe() {
 	this.w = 80;
 	this.speed = 5;
 
+	this.highlight = false;
+
 	this.hits = function(bird) {
 		if (bird.y < this.top || bird.y > height - this.bottom) {
 			if (bird.x > this.x && bird.x < this.x + this.w) {
 				return true;
 			}
 		}
-
+		this.highlight = false;
 		return false;
 	};
 
@@ -25,6 +27,9 @@ function Pipe() {
 
 	this.show = function() {
 		fill(255);
+		if (this.highlight) {
+			fill(255, 0, 0);
+		}
 		rect(this.x, 0, this.w, this.top);
 		rect(this.x, height - this.bottom, this.w, this.bottom);
 	};
